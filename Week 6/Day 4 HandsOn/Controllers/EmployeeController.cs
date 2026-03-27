f0a109cf800838bc525d04cdb0509a011a4c87de
raju k <rajappakankanawadi@gmail.com>
2026-03-27 09:48:56 +0530

    Added Week 6 Day 4 Files

---
+++ Week 6/Day 4 HandsOn/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee { Empno = 101, Ename = "Raju", Job = "Developer", Salary = 50000, Deptno = 10 },
+                new Employee { Empno = 102, Ename = "Anil", Job = "Tester", Salary = 40000, Deptno = 20 },
+                new Employee { Empno = 103, Ename = "Kiran", Job = "Manager", Salary = 70000, Deptno = 10 },
+                new Employee { Empno = 104, Ename = "Sneha", Job = "HR", Salary = 35000, Deptno = 30 },
+                new Employee { Empno = 105, Ename = "Priya", Job = "Developer", Salary = 55000, Deptno = 20 }
+            };
+
+            return View(employees);
+        }
+    }
+}
+
+
---
+++ Week 6/Day 4 HandsOn/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private static List<Product> products = new List<Product>()
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 60000, Category = "Electronics" },
+            new Product { Id = 2, Name = "Mobile", Price = 20000, Category = "Electronics" },
+            new Product { Id = 3, Name = "Shoes", Price = 3000, Category = "Fashion" },
+            new Product { Id = 4, Name = "Watch", Price = 5000, Category = "Accessories" }
+        };
+        public IActionResult Index()
+        {
+
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+    }
+}
