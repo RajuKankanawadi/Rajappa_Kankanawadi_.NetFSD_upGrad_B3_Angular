9ad924684832c00aa23f15a8f46d5e78042d84c2
Rajappa Kankanawadi <150230556+RajuKankanawadi@users.noreply.github.com>
2026-03-30 17:32:16 +0530

    Week 6 Day 5 files

---
+++ Week 6/Day 5 hands_on_Product/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+
+        [Required(ErrorMessage = "Product Id is required")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Product Name should be between 5 and 15 characters")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category should be between 5 and 15 characters")]
+        public string Category { get; set; }
+    }
+}
