ef0fa1d149a7fbcc2f029347521557c06eabcc12
Rajappa Kankanawadi <150230556+RajuKankanawadi@users.noreply.github.com>
2026-03-30 17:35:46 +0530

    Week 7 Day 1 Files

---
+++ Week 6/Day 5 hands_on_Product/Views/Week 7/Day 1_ContactInfo_hands_on/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ContactController : Controller
+        {
+            private static List<ContactInfo> contacts = new List<ContactInfo>()
+            {
+            new ContactInfo
+            {
+                ContactId = 1,
+                FirstName = "Ravi",
+                LastName = "Kumar",
+                CompanyName = "ABC Pvt Ltd",
+                EmailId = "ravi@gmail.com",
+                MobileNo = 9876543210,
+                Designation = "Manager"
+            },
+            new ContactInfo
+            {
+                ContactId = 2,
+                FirstName = "Raghu",
+                LastName = "Patil",
+                CompanyName = "XYZ Technologies",
+                EmailId = "sneha@gmail.com",
+                MobileNo = 9876501234,
+                Designation = "Developer"
+            }
+        };
+
+            // Display all contacts
+            public ActionResult ShowContacts()
+            {
+                return View(contacts);
+            }
+
+            // Search contact by Id
+            public ActionResult GetContactById(int id)
+            {
+                ContactInfo contact = contacts.FirstOrDefault(c => c.ContactId == id);
+
+                return View(contact);
+            }
+
+            // GET: Add Contact
+            public ActionResult AddContact()
+            {
+                return View();
+            }
+
+            // POST: Add Contact
+            [HttpPost]
+            public ActionResult AddContact(ContactInfo contactInfo)
+            {
+                contacts.Add(contactInfo);
+
+                return RedirectToAction("ShowContacts");
+            }
+        }
+    }
---
+++ Week 6/Day 5 hands_on_Product/Views/Week 7/Day 1_ContactInfo_hands_on/Models/ContactInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class ContactInfo
+    {
+        [Required(ErrorMessage = "Contact Id is required")]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "First Name should be between 5 and 15 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Last Name should be between 5 and 15 character")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "EmailId is required")]
+        public string EmailId { get; set; }
+
+        [Required (ErrorMessage = "Mobile Number should be 10 Numbers Only")]
+        public long MobileNo { get; set; }
+
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+    }
+}
