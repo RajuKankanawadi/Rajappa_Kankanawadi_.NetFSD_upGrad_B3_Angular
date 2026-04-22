bb520644d4698d1192e12347c8d02b7aecc281f5
raju k <rajappakankanawadi@gmail.com>
2026-04-22 17:48:05 +0530

    Week 10 Day 3 Updated Files

--- Week 9/Day 4/ApiGateWay1/obj/Debug/net10.0/ApiGateWay1.AssemblyInfo.cs
+++ Week 9/Day 4/ApiGateWay1/obj/Debug/net10.0/ApiGateWay1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateWay1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d21f34a25e52a2dc6a9eb332d34cb56d2d8275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b24887e00b4fcc9026d3be3078573f08eb71c719")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateWay1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateWay1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
